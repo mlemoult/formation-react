7df4a524367f4453766e0b1fedf36ba4640cace6
mlemoult <mlemoult@loir-et-cher-logement.fr>
2025-12-30 10:36:23 +0100

    Refortoring

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,24 +1,11 @@
-import React, { useState } from 'react';
-import Button from '../component/Button';
+import LoginForm from '../component/LoginForm';
 
 const LoginPage = () => {
 
-    const [prenom, setPrenom] = useState('')
-
-    const handleSubmit = () => {
-        alert(prenom)
-        setPrenom('')
-    }
 
     return (
         <>
-            <h1>Bienvenue chez nous !</h1>
-
-            <h2>Connectez-vous</h2>
-            <form onSubmit={handleSubmit}>
-                <input type='text' placeholder='Entrez votre prénom...' required onChange={(e) => setPrenom(e.target.value)} />
-                <Button label='Entrez votre prénom...' />
-            </form>
+            <LoginForm />
         </>
     );
 };
